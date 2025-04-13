246542b21f2857bf03102b3ac9929269042072ac
0xmusashi <musashihp42@gmail.com>
2025-04-14 01:18:33 +0700

    use relayer api hosted on cloud

--- packages/adapters/solana/src/providers/constants.ts
+++ packages/adapters/solana/src/providers/constants.ts
@@ -1,6 +1,6 @@
-export const RELAYER_URL = 'http://localhost:4000/api/v1'
+export const RELAYER_URL = 'http://147.93.135.117:4000/api/v1'
 export const RELAYER_SPL_MARGIN = 0.01
 export const RELAYER_SPL_TOKEN_PRICE_INFO = {
   // use fixed price now, use auto-fetch jup price in relayer service later
-  price: 1000000000, // 1 token = 1 SOL (decimals = 9)
+  price: 1000000000 // 1 token = 1 SOL (decimals = 9)
 }
